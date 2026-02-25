4473ac59a23c10a63a3d7c4f0755898184c38c12
u240476 <joshua.reynolds.2024@mumail.ie>
2026-02-25 20:35:03 +0000

    2nd commit
    noticed that valid doubles array was not final, made that change to avoid potential overwriting later on

--- Doubles.java
+++ Doubles.java
@@ -15,7 +15,7 @@ public class Doubles{
      * @return boolean
      */
     public static boolean checkingValidDoubles(int dart){
-        int[] validDoubles = {2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40,50}; 
+        final int[] validDoubles = {2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40,50}; 
         for(int i: validDoubles){
             if(dart == i){
                 return true;
